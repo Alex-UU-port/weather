f9686673fd5d864ce6d0ed94e29cc97207d8ffa6
AlexMosk <jamos2108@gmail.com>
2025-11-02 17:07:59 +0800

    finish commit

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,50 @@
+import java.util.Scanner;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
     public static void main (String[] args) throws Exception {
-        System.out.println("Hello");
 
-        float lat = 50.5f;
-        float lon = 107.37f;
+        System.out.println("Координаты Москвы: 50.45 С.Ш. 37.37 В.Д.");
 
-        String json = getWeather.get(lat, lon);
-        System.out.println("Ответ API: " + json);
+        float lat = 50.45f;
+        float lon = 37.37f;
 
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode rootNode = mapper.readTree(json);
+            String json = getWeather.get(lat, lon);
+            System.out.println("Ответ API: " + json);
 
-        if (!rootNode.isMissingNode()) {
-            int currentTemp = rootNode.path("fact").path("temp").asInt();
-            System.out.println("\nТекущая температура: " + currentTemp + "°C");
-        } else {
-            System.out.println("Данные о текущей погоде отсутствуют");
-        }
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(json);
 
-        JsonNode forecasts = rootNode.path("forecasts");
-        if (forecasts.isArray() && forecasts.size() > 0) {
-            double sumTemp = 0;
-            int count = 0;
-            for (JsonNode forecast : forecasts) {
-                JsonNode parts = forecast.path("parts");
-                JsonNode day = parts.path("day");
-                if (!day.isMissingNode() && day.has("temp_avg")) {
-                    sumTemp += day.get("temp_avg").asDouble();
-                    count++;
-                }
-            }
-            if (count > 0) {
-                double avgTemp = sumTemp / count;
-                System.out.printf("Средняя температура за %d дней: %.2f°C\n", count, avgTemp);
+            if (!rootNode.isMissingNode()) {
+                int currentTemp = rootNode.path("fact").path("temp").asInt();
+                System.out.println("\nТекущая температура: " + currentTemp + "°C");
             } else {
-                System.out.println("Нет данных о дневных температурах для расчета");
+                System.out.println("Данные о текущей погоде отсутствуют");
             }
-        } else {
-            System.out.println("Отсутствуют прогнозы для вычисления средней температуры");
-        }
+
+            JsonNode forecasts = rootNode.path("forecasts");
+            if (forecasts.isArray() && forecasts.size() > 0) {
+                double sumTemp = 0;
+                int count = 0;
+                for (JsonNode forecast : forecasts) {
+                    JsonNode parts = forecast.path("parts");
+                    JsonNode day = parts.path("day");
+                    if (!day.isMissingNode() && day.has("temp_avg")) {
+                        sumTemp += day.get("temp_avg").asDouble();
+                        count++;
+                    }
+                }
+                if (count > 0) {
+                    double avgTemp = sumTemp / count;
+                    System.out.printf("Средняя температура за %d дней: %.2f°C\n", count, avgTemp);
+                } else {
+                    System.out.println("Нет данных о дневных температурах для расчета");
+                }
+            } else {
+                System.out.println("Отсутствуют прогнозы для вычисления средней температуры");
+            }
+
 
 
     }
